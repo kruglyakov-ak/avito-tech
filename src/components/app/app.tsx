c02d3150b0e2d27757bab9b1d4401e3aa061ef20
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2023-01-10 20:55:18 +0300

    setup routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { APP_ROUTS } from "../../const";
+import StoriesList from "../stories-list/stories-list";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path={APP_ROUTS.Main}>
+          <StoriesList />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
